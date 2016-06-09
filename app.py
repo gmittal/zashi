a1f55b4f91a03b8f2217fd8850857aabe082bfa6
gmittal <gautam@mittal.net>
2016-06-09 08:55:41 -0700

    updates

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import numpy as np
 app = Flask(__name__)
 
-@app.route("/")
+def sigmoid(score):
+    return 1/(1+np.exp(-score))
+
+@app.route("/", methods=["GET"])
 def hello():
     return "Hello World!"
 
+@app.route("/read", methods=["POST"])
+def read():
+    return "Something"
+
 if __name__ == "__main__":
+    print sigmoid(-0.5)
     app.run()
